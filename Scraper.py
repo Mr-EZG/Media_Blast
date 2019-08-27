34c28e00cf24627b3c6153655f3c2944bb9b9815
Ethan Garza <ethangarza@Ethans-MacBook-Air-9.local>
2019-08-26 22:46:42 -0400

    updated authentication

--- Scraper.py
+++ Scraper.py
@@ -2,3 +2,14 @@ import tweepy
 from credentials import *
 
 keys = Keys()
+
+import tweepy
+
+auth = tweepy.OAuthHandler(keys.APIKey, keys.APIsecret)
+auth.set_access_token(keys.accessToken, keys.accessSecretToken)
+
+api = tweepy.API(auth)
+
+public_tweets = api.home_timeline()
+for tweet in public_tweets:
+    print(tweet.text)
