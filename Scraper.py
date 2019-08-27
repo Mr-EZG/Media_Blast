a4434e3009598ed907b96158ee5e6125d4ccc23f
Ethan Garza <ethangarza@Ethans-MacBook-Air-9.local>
2019-08-27 00:49:49 -0400

    new updated files with woeids

--- Scraper.py
+++ Scraper.py
@@ -8,12 +8,32 @@ auth.set_access_token(keys.accessToken, keys.accessSecretToken)
 
 api = tweepy.API(auth)
 
-search = api.search(q="hello", rpp="3", count="2", result_type="popular", tweet_mode="extended")
+trends_place = api.trends_available()
+print(trends_place)
+print()
+print("---------------------")
+# 2367105
+count = 0
+for trend in trends_place:
+    print(trend)
+    print("City:", trend["name"])
+    print("Country:", trend["country"])
+    print("Woeid:", trend["woeid"])
+    print()
+    count += 1
+
+print("count:", count)
+
+# Portland
+# trends = api.trends_closest(lat="27.88168", long="-97.31916")
+# print(trends, "\n\n")
+
+search = api.search(q="hello", count="2", result_type="popular", tweet_mode="extended", lang="en")
 
 # public_tweets = api.home_timeline()
 # print(api.rate_limit_status())
-print("\n\n--------------\n\n")
-print(search)
+# print("\n\n--------------\n\n")
+# print(search)
 print("\n\n--------------\n\n")
 for query in search:
     print(query.full_text)
