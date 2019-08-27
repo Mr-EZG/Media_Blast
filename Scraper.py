cc2436f94229308785e089d0b985ec728fe0398f
Ethan Garza <ethangarza@Ethans-MacBook-Air-9.local>
2019-08-26 23:36:11 -0400

    attributes

--- Scraper.py
+++ Scraper.py
@@ -10,13 +10,13 @@ auth.set_access_token(keys.accessToken, keys.accessSecretToken)
 
 api = tweepy.API(auth)
 
-search = api.search(q="hello world", rpp="3", count="2", result_type="popular")
+search = api.search(q="hello", rpp="3", count="2", result_type="popular", tweet_mode="extended")
 
 # public_tweets = api.home_timeline()
-print(api.rate_limit_status())
+# print(api.rate_limit_status())
 print("\n\n--------------\n\n")
 print(search)
 print("\n\n--------------\n\n")
 for query in search:
-    print(query.text)
+    print(query.full_text)
     print("\n")
