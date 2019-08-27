a328e5ba883664a8c24f068c6b57355e0aa64431
Namit-Agrawal <namit.agrawal20@gmail.com>
2019-08-26 21:29:18 -0500

    File

---
+++ credentials.py
@@ -0,0 +1,7 @@
+
+class Keys(object):
+      def __init__(self, APIKey, APIsecret, accessToken, accessSecretToken):
+        self.APIKey = APIKey
+        self.APIsecret = APIsecret
+        self.accessToken = accessToken
+        self.accessSecretToken = accessSecretToken
